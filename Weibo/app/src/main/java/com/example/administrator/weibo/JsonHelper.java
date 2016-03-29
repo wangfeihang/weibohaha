cf456c33fd5d52534d2b7c1cb710166c1435659f
ZZB <realbo89@outlook.com>
2016-03-29 20:04:39 +0800

    JsonHelper添加方法

--- Weibo/app/src/main/java/com/example/administrator/weibo/JsonHelper.java
+++ Weibo/app/src/main/java/com/example/administrator/weibo/JsonHelper.java
@@ -1,14 +1,35 @@
 package com.example.administrator.weibo;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
+import java.lang.reflect.Type;
 
 /**
  * Created by Administrator on 2016/3/28.
  */
 public class JsonHelper {
+    private static Gson sGson;
+    private static Gson getGson() {
+        if (sGson == null) {
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            sGson = gsonBuilder.create();
+        }
+        return sGson;
+    }
     public static <T>T toObject(String jsonString,Class<T> mclass)
     {
         Gson gson=new Gson();
         return gson.fromJson(jsonString, mclass);
+    }
+    public static <T> T jsonToObject(String json, Type tokenType) {
+        T t = null;
+        try {
+            t = getGson().fromJson(json, tokenType);
+        }catch (JsonSyntaxException e){
+            e.printStackTrace();
+        }
+        return t;
     }
 }
