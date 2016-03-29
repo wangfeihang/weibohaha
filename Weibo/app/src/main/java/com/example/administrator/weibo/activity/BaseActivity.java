48d7e39c47277dc0b8bf3bfcb1c0d7cab2a9440a
ZZB <realbo89@outlook.com>
2016-03-29 16:32:04 +0800

    Activity基类

---
+++ Weibo/app/src/main/java/com/example/administrator/weibo/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.administrator.weibo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZZB on 2016/3/29.
+ */
+public class BaseActivity extends AppCompatActivity {
+    
+}
