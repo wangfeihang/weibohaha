1e7c403b38ba426279e18a1046cb9073f539f50b
ZZB <realbo89@outlook.com>
2016-03-29 16:34:35 +0800

    添加微博授权Activity, 注意Activity layout资源文件命名，是以activity开关，然后再用类名小写组成

---
+++ Weibo/app/src/main/java/com/example/administrator/weibo/activity/AuthActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.weibo.activity;
+
+import android.os.Bundle;
+
+import com.example.administrator.weibo.R;
+
+/**
+ * Created by ZZB on 2016/3/29.
+ */
+public class AuthActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
