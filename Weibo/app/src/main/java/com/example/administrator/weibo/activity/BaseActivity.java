752fbff65f5e34e1f563831cc217fedba00e8508
ZZB <realbo89@outlook.com>
2016-03-29 17:29:58 +0800

    基类加DI.inject

--- Weibo/app/src/main/java/com/example/administrator/weibo/activity/BaseActivity.java
+++ Weibo/app/src/main/java/com/example/administrator/weibo/activity/BaseActivity.java
@@ -1,10 +1,18 @@
 package com.example.administrator.weibo.activity;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.yy.androidlib.di.DI;
 
 /**
  * Created by ZZB on 2016/3/29.
  */
 public class BaseActivity extends AppCompatActivity {
-    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DI.inject(this);
+    }
 }
